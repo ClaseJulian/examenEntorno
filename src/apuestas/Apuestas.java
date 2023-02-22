17dbd554d25727c3e18b2439a4c17513390ef900
Julian <jmorcue0405@g.educaand.es>
2023-02-22 12:06:55 +0100

    terminado

--- src/apuestas/Apuestas.java
+++ src/apuestas/Apuestas.java
@@ -8,25 +8,31 @@ package apuestas;
 public class Apuestas {
 
     public static void main(String[] args) {
-        Apuesta miApuesta;
+        Apuesta laApuesta;
         int mi_dinero;
 
-        miApuesta = new Apuesta(1000, 4, 2);
-        try {
-            System.out.println("Apostando...");
-            miApuesta.apostar(25);
-        } catch (Exception e) {
-            System.out.println("Fallo al realizar la Apuesta");
-        }
+        laApuesta = operativa_Apuesta(25);
 
         try {
             System.out.println("Intento cobrar apuesta segun el resultado del partido");
-            miApuesta.cobrar_apuesta(2, 3);
+            laApuesta.cobrar_apuesta(2, 3);
         } catch (Exception e) {
             System.out.println("Fallo al cobrar la apuesta");
         }
-        mi_dinero = miApuesta.getDinero_disp();
+        mi_dinero = laApuesta.getDinero_disp();
         System.out.println("El dinero que tengo tras las apuestas es " + mi_dinero);
+    }
+
+    private static Apuesta operativa_Apuesta(int dinero) {
+        Apuesta laApuesta;
+        laApuesta = new Apuesta(1000, 4, 2);
+        try {
+            System.out.println("Apostando...");
+            laApuesta.apostar(dinero);
+        } catch (Exception e) {
+            System.out.println("Fallo al realizar la Apuesta");
+        }
+        return laApuesta;
     }
     
 }
